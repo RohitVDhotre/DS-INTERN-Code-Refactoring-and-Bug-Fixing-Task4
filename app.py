82b58cf1e3efe0209b14938c4efea650ecf4e446
RohitVDhotre <93522974+RohitVDhotre@users.noreply.github.com>
2024-02-28 14:25:47 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask,render_template,redirect,request,url_for
+
+app = Flask(__name__)
+
+notes=[]
+
+@app.route("/",methods=["GET","POST"])
+def index():
+    if request.method == "POST":
+        note = request.form["note"]
+        if note:
+            notes.append(note)
+            return redirect(url_for('index'))
+    return render_template('home.html', notes=notes)
+
+
+
+if __name__ == "__main__":
+    app.run(debug = True)
